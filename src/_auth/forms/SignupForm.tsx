e2579d750df9ca15267549bb43db5a64a9b8fc8c
Luca <lucanegresco@gmail.com>
2023-10-29 21:49:25 -0300

    S

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -13,12 +13,25 @@ import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import { Loader } from "@/components/shared";
-import { Link } from "react-router-dom";
-import { createUserAccount } from "@/lib/appwrite/api";
+import { Link, useNavigate } from "react-router-dom";
+
+import { useToast } from "@/components/ui/use-toast";
+import {
+  useCreateUserAccount,
+  useSignInAccount,
+} from "@/lib/react-query/queriesAndMutations";
+import { useUserContext } from "@/context/AuthContext";
 
 const SignupForm = () => {
-  const isLoading = false;
+  const { toast } = useToast();
+  const navigate = useNavigate();
+  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
 
+  const { mutateAsync: createUserAccount, isPending: isCreatingUser } =
+    useCreateUserAccount();
+
+  const { mutateAsync: signInAccount, isPending: isSigningIn } =
+    useSignInAccount();
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -34,7 +47,30 @@ const SignupForm = () => {
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     const newUser = await createUserAccount(values);
 
-    console.log(newUser);
+    if (!newUser) {
+      return toast({
+        title: "Signup Error, Please Try Again",
+      });
+    }
+
+    const session = await signInAccount({
+      email: values.email,
+      password: values.password,
+    });
+
+    if (!session) {
+      return toast({ title: "Sign in failed, Please try again." });
+    }
+
+    const isLoggedIn = await checkAuthUser();
+
+    if (isLoggedIn) {
+      form.reset();
+
+      navigate("/");
+    } else {
+      return toast({ title: "Sign up failed, Please try again." });
+    }
   }
 
   return (
@@ -106,11 +142,11 @@ const SignupForm = () => {
             )}
           />
           <Button
-            disabled={isLoading}
+            disabled={isCreatingUser}
             type="submit"
             className="shad-button_primary"
           >
-            {isLoading ? (
+            {isCreatingUser ? (
               <div className="flex-center gap-2">
                 <Loader />
                 Loading...
--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { Bottombar, LeftSidebar, Topbar } from "@/components/shared";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <div className="w-full md:flex">
+      <Topbar />
+      <LeftSidebar />
+
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <Bottombar />
+    </div>
+  );
 };
 
 export default RootLayout;
